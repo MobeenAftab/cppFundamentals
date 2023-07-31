62abcc036a38dc2c2db925079ef1f22d909188b0
Mobeen Aftab <MobeenAftab@users.noreply.github.com>
2023-07-31 12:29:40 +0100

    add sprite with time based animation control

--- dapper-dasher/main.cpp
+++ dapper-dasher/main.cpp
@@ -7,8 +7,8 @@ int main()
 
     InitWindow(windowWidth, windowHeight, "Dapper Dasher");
 
-    // acceleration due to gravity (pixel/frames)/fame
-    const int gravity{1};
+    // acceleration due to gravity (pixel/second)/second
+    const int gravity{1'000};
 
     Texture2D scarfy = LoadTexture("textures/scarfy.png");
     Rectangle scarfyRec;
@@ -21,9 +21,18 @@ int main()
     scarfyPos.x = windowWidth / 2 - scarfyRec.width / 2;
     scarfyPos.y = windowHeight - scarfy.height;
 
+    // animimation frame
+    int frame{};
+
     int velocity{0};
-    int jumpValue{-22};
+    // (pixel/second)
+    int jumpValue{-600};
     bool isInAir{};
+
+    // amount of time before we update the animation time
+    // cycle through sprite sheet once every 12th of a second
+    const float updateTime{1.0 / 12.0};
+    float runnningTime{};
 
     SetTargetFPS(60);
 
@@ -31,6 +40,9 @@ int main()
     {
         BeginDrawing();
         ClearBackground(RAYWHITE);
+
+        // delta time (time since last frame)
+        const float dT = GetFrameTime();
 
         // ground check
         if (scarfyPos.y >= windowHeight - scarfyRec.height)
@@ -40,7 +52,7 @@ int main()
         }
         else
         {
-            velocity += gravity;
+            velocity += gravity * dT;
             isInAir = true;
         }
 
@@ -49,7 +61,24 @@ int main()
             velocity += jumpValue;
         }
 
-        scarfyPos.y += velocity;
+        // update the position
+        scarfyPos.y += velocity * dT;
+
+        // update running time
+        runnningTime += dT;
+
+        if (runnningTime >= updateTime)
+        {
+            runnningTime = 0;
+            // update animation frame
+            scarfyRec.x = frame * scarfyRec.width;
+            frame++;
+            if (frame > 5)
+            {
+                frame = 0;
+            }
+        }
+
         DrawTextureRec(scarfy, scarfyRec, scarfyPos, WHITE);
 
         EndDrawing();
