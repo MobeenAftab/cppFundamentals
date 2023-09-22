9471fbbf9a594d4b1e3fee19a77357e2aaebfcf7
Mobeen Aftab <MobeenAftab@users.noreply.github.com>
2023-09-22 09:32:24 +0100

    Draw knight character texture

--- classy_clash/main.cpp
+++ classy_clash/main.cpp
@@ -5,13 +5,24 @@ int main(void)
 {
     const int screenWidth = 384;
     const int screenHeight = 384;
+    const float speed = 4.0f;
 
     InitWindow(screenWidth, screenHeight, "Classy Clash");
 
     Texture2D map = LoadTexture("nature_tileset/OpenWorldMap24x24.png");
     Vector2 mapPos = {0.0, 0.0};
 
-    const float speed = 4.0;
+    Texture2D knight = LoadTexture("characters/knight_idle_spritesheet.png");
+    Vector2 knightPos{
+        (float)screenWidth / 2.0f - 4.0f * (0.5f * (float)knight.width / 6.0f),
+        (float)screenHeight / 2.0f - 4.0f * (0.5f * (float)knight.height)};
+    Rectangle knightRec{
+        0.0f,
+        0.0f,
+        (float)knight.width / 6.0f,
+        (float)knight.height};
+    Rectangle knightDest{
+        knightPos.x, knightPos.y, 4.0f * (float)knight.width / 6.0f, 4.0f * (float)knight.height};
 
     SetTargetFPS(60);
     while (!WindowShouldClose())
@@ -40,10 +51,13 @@ int main(void)
 
         DrawTextureEx(map, mapPos, 0.0, 4.0, WHITE);
 
+        DrawTexturePro(knight, knightRec, knightDest, Vector2{}, 0.0f, WHITE);
+
         EndDrawing();
     }
 
     UnloadTexture(map);
+    UnloadTexture(knight);
     CloseWindow();
 
     return 0;
